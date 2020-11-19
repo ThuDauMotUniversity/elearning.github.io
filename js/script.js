41bf2238df1de7d9c3d851d7c19a7f38d3163623
Bui Van Xia <buivanxia10@gmail.com>
2020-11-19 22:03:40 +0700

    update new code

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function facebookClick() {
+    window.open("https://www.facebook.com/tdmuelearning");
+};
+
+function youtubeClick(){
+    window.open("https://www.youtube.com/c/ELearning%C4%90%E1%BA%A1ih%E1%BB%8DcTh%E1%BB%A7D%E1%BA%A7uM%E1%BB%99t/");
+}
+
+function mailClick(){
+    window.location.href = "mailto:elearning@tdmu.edu.vn";
+}
+
+function officeClick(){
+    window.open("https://office.com");
+}
